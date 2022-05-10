2964078bf1ef2eaf2e7c64183f7dcd704572f02b
Everton Riconi <evertonriconi@gmail.com>
2022-05-10 16:16:14 -0300

    Alteracoes app

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,15 +8,36 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.transparent,
+        backgroundColor: Color.fromARGB(255, 229, 163, 8),
         elevation: 0,
         title: const Text(
           'Flutter Internationalization',
           style: TextStyle(color: Colors.black54),
         ),
       ),
-      body: Center(
-        child: Text(AppLocalizations.of(context)!.message),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+            child: Column(
+          children: [
+            Image.asset(
+              'assets/logo.png',
+              height: 230,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Text(AppLocalizations.of(context)!.message,
+                style: const TextStyle(fontSize: 20, color: Colors.black)),
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              AppLocalizations.of(context)!.language,
+              style: const TextStyle(fontSize: 25, color: Colors.black),
+            )
+          ],
+        )),
       ),
     );
   }
