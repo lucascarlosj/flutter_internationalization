c8cc4e79fb93a25730430358fa9b3a307c6549b7
Lucas Carlos <luc.cjov@rede.ulbra.br>
2022-05-10 19:44:42 -0300

    partial commit

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -13,14 +13,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        title: const Text(
-          'Flutter Internationalization',
-          style: TextStyle(
-            color: Colors.black54,
-          ),
-        ),
+        title: const Text('Flutter Internationalization'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(20),
@@ -29,28 +22,18 @@ class _HomePageState extends State<HomePage> {
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              const SizedBox(
-                height: 30,
-              ),
-              Image.asset(
-                'assets/logo.png',
-                scale: 1.5,
+              Image.network(
+                AppLocalizations.of(context)!.language.toString(),
+                scale: 4,
               ),
               const SizedBox(
-                height: 30,
+                height: 60,
               ),
               Text(
                 AppLocalizations.of(context)!.message,
                 textAlign: TextAlign.justify,
-                style: const TextStyle(fontSize: 20, color: Colors.black54),
+                style: const TextStyle(fontSize: 16, color: Colors.black54),
               ),
-              const SizedBox(
-                height: 30,
-              ),
-              Image.network(
-                AppLocalizations.of(context)!.language.toString(),
-                scale: 3,
-              )
             ],
           ),
         ),
